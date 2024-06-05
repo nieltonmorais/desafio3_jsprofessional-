10d875c732e02a99450204d79591ac3a12dbf5f0
Nielton Morais <nielton.morais@gmail.com>
2024-06-04 22:06:36 -0300

    Save, Update, Delete

--- src/main/java/com/devsuperior/desafiocrud/controllers/ClientController.java
+++ src/main/java/com/devsuperior/desafiocrud/controllers/ClientController.java
@@ -1,16 +1,14 @@
 package com.devsuperior.desafiocrud.controllers;
 
 import com.devsuperior.desafiocrud.DTO.ClientDTO;
+import com.devsuperior.desafiocrud.entities.Client;
 import com.devsuperior.desafiocrud.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.NoSuchElementException;
 
@@ -41,6 +39,32 @@ public class ClientController {
         } catch (NoSuchElementException e) {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientDTO> save(@RequestBody Client client){
+        try {
+            ClientDTO clientDTO = service.save(client);
+            return ResponseEntity.ok(clientDTO);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(400).build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO client){
+        try {
+            ClientDTO clientDTO = service.update(id, client);
+            return ResponseEntity.ok(clientDTO);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(400).build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/com/devsuperior/desafiocrud/services/ClientService.java
+++ src/main/java/com/devsuperior/desafiocrud/services/ClientService.java
@@ -8,6 +8,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.NoSuchElementException;
 
@@ -28,6 +30,29 @@ public class ClientService {
         Client client = repository.findById(id)
                 .orElseThrow(() -> new NoSuchElementException("Client not found with id " + id));
         return new ClientDTO(client);
+    }
+
+    @Transactional
+    public ClientDTO save(Client client) {
+        Client cli = repository.save(client);
+        return new ClientDTO(cli);
+    }
+
+    @Transactional
+    public ClientDTO update(Long id, ClientDTO client) {
+        Client cli = repository.getReferenceById(id);
+        cli.setName(client.getName());
+        cli.setCpf(client.getCpf());
+        cli.setBirthDate(client.getBirthDate());
+        cli.setIncome(client.getIncome());
+        cli.setChildren(client.getChildren());
+        cli = repository.save(cli);
+        return new ClientDTO(cli);
+    }
+
+    @DeleteMapping
+    public void delete(Long id){
+        repository.deleteById(id);
     }
 }
 
